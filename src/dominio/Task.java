d30f32ce96adec35159150e04d09d296356cfb5b
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-02 20:19:17 -0300

    Arrumado a mensagem exibida ao mostrar o período de uma tarefa.

--- src/dominio/Task.java
+++ src/dominio/Task.java
@@ -19,7 +19,7 @@ public class Task {
     public String toString() {
         return "Título: " + this.title + "\nDescrição: " + this.description +
                 "\nData da realização da tarefa: " + this.validity +
-                "\nPeríodo da realização da tarefa: período da " + this.period.getDayPeriod();
+                "\nPeríodo da realização da tarefa: período da " + this.period.getDayPeriod() + ".";
     }
 
     public String getTitle() {
