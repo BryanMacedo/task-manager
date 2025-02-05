aca4987f2ba551d0927b9825728827eb188ed50b
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-05 19:00:38 -0300

    Utilizei o try/catch para fazer o programa não encerrar ao passar uma opção invalida durante a escolha da funcionalidade desejada.

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,7 @@ import dominio.Task;
 import service.TaskManager;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,36 +16,41 @@ public class Main {
         int choice = 0;
 
         while (choice != 6) {
-            System.out.println("1 - Adicionar uma tarefa");
-            System.out.println("2 - Listar tarefas");
-            System.out.println("3 - Excluir uma tarefa");
-            System.out.println("4 - Salvar lista de tarefas");
-            System.out.println("5 - Editar uma tarefa");
-            System.out.println("6 - Encerrar o programa");
-            System.out.print("Escolha: ");
-            choice = sc.nextInt();
+            try {
+                System.out.println("1 - Adicionar uma tarefa");
+                System.out.println("2 - Listar tarefas");
+                System.out.println("3 - Excluir uma tarefa");
+                System.out.println("4 - Salvar lista de tarefas");
+                System.out.println("5 - Editar uma tarefa");
+                System.out.println("6 - Encerrar o programa");
+                System.out.print("Escolha: ");
+                choice = sc.nextInt();
 
-            switch (choice) {
-                case 1 -> {
-                    taskManager.addTask();
+                switch (choice) {
+                    case 1 -> {
+                        taskManager.addTask();
+                    }
+                    case 2 -> {
+                        taskManager.listTasks();
+                    }
+                    case 3 -> {
+                        taskManager.removeTask();
+                    }
+                    case 4 -> {
+                        taskManager.saveTasks();
+                    }
+                    case 5 -> {
+                        taskManager.editTasks();
+                    }case 6 -> {
+                        System.out.println("\nEncerrando o programa.");
+                    }
+                    default -> {
+                        throw new InputMismatchException();
+                    }
                 }
-                case 2 -> {
-                    taskManager.listTasks();
-                }
-                case 3 -> {
-                    taskManager.removeTask();
-                }
-                case 4 -> {
-                    taskManager.saveTasks();
-                }
-                case 5 -> {
-                    taskManager.editTasks();
-                }case 6 -> {
-                    System.out.println("\nEncerrando o programa.");
-                }
-                default -> {
-                    System.out.println("\nOpção não encontrada, escolha uma opção valida.\n");
-                }
+            }catch (InputMismatchException e){
+                System.out.println("\nOpção inválida, por favor informe o número da opção desejada.\n");
+                sc.nextLine();
             }
         }
         sc.close();
